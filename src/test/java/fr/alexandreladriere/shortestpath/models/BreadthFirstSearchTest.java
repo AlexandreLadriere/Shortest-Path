58ec843ad7c075da260ffbb66e4e56c4887e12cf
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2020-04-15 16:46:37 +0200

    add: Documentation and path length test

--- src/test/java/fr/alexandreladriere/shortestpath/models/BreadthFirstSearchTest.java
+++ src/test/java/fr/alexandreladriere/shortestpath/models/BreadthFirstSearchTest.java
@@ -18,42 +18,70 @@ import static org.junit.Assert.assertEquals;
 public class BreadthFirstSearchTest {
     private int[][] matrix;
     private int[] expectedStartingPoint;
+    private int expectedPathLength;
 
-    public BreadthFirstSearchTest(int[][] matrix, int[] expectedStartingPoint) {
+    /**
+     * Default constructor
+     *
+     * @param matrix                Matrix that you want to run the test with
+     * @param expectedStartingPoint Expected starting in the matrix that you want to run the test with
+     * @param expectedPathLength    Expected shortest path length in the matrix
+     */
+    public BreadthFirstSearchTest(int[][] matrix, int[] expectedStartingPoint, int expectedPathLength) {
         super();
         initMatrix(matrix);
         this.expectedStartingPoint = new int[2];
         this.expectedStartingPoint[0] = expectedStartingPoint[0];
         this.expectedStartingPoint[1] = expectedStartingPoint[1];
+        this.expectedPathLength = expectedPathLength;
     }
 
+    /**
+     * Initialize the inputs of the test
+     *
+     * @return Collection of input
+     * @see BreadthFirstSearch
+     */
     @Parameterized.Parameters
     public static Collection input() {
         return Arrays.asList(new Object[][]{
+                // default
                 {new int[][]{
                         {Constants.EMPTY, Constants.START, Constants.EMPTY, Constants.EMPTY},
                         {Constants.EMPTY, Constants.OBSTACLE, Constants.OBSTACLE, Constants.OBSTACLE},
                         {Constants.EMPTY, Constants.EMPTY, Constants.OBSTACLE, Constants.EMPTY},
                         {Constants.EMPTY, Constants.EMPTY, Constants.EMPTY, Constants.END}
                 },
-                        new int[]{0, 1}},
+                        new int[]{0, 1},
+                        7
+                },
+                // 2 starting points
                 {new int[][]{
                         {Constants.EMPTY, Constants.EMPTY, Constants.START, Constants.EMPTY},
                         {Constants.EMPTY, Constants.OBSTACLE, Constants.OBSTACLE, Constants.OBSTACLE},
                         {Constants.EMPTY, Constants.EMPTY, Constants.START, Constants.EMPTY},
                         {Constants.EMPTY, Constants.EMPTY, Constants.EMPTY, Constants.END}
                 },
-                        new int[]{0, 2}},
+                        new int[]{0, 2},
+                        8
+                },
+                // No starting point
                 {new int[][]{
                         {Constants.EMPTY, Constants.EMPTY, Constants.EMPTY, Constants.EMPTY},
                         {Constants.EMPTY, Constants.OBSTACLE, Constants.OBSTACLE, Constants.OBSTACLE},
                         {Constants.EMPTY, Constants.EMPTY, Constants.OBSTACLE, Constants.EMPTY},
                         {Constants.EMPTY, Constants.EMPTY, Constants.EMPTY, Constants.END}
                 },
-                        new int[]{0, 0}},
+                        new int[]{0, 0},
+                        6},
         });
     }
 
+    /**
+     * Initialize the matrix parameter
+     *
+     * @param matrix The matrix that you want to run the test with
+     */
     private void initMatrix(int[][] matrix) {
         this.matrix = new int[matrix.length][matrix[0].length];
         for (int i = 0; i < matrix.length; i++) {
@@ -70,6 +98,6 @@ public class BreadthFirstSearchTest {
         int col_s = BreadthFirstSearch.findStartingPoint(matrix)[1];
         assertEquals(expectedStartingPoint[0], row_s);
         assertEquals(expectedStartingPoint[1], col_s);
-        // TODO Test with another matrix without a starting point, or with many starting points
+        assertEquals(expectedPathLength, BreadthFirstSearch.shortestPathLength(matrix));
     }
 }
