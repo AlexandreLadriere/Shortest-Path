bb125675521a4d15d3820b5b2b4d836008bc07c4
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2020-04-15 15:32:01 +0200

    add: BFS distance
    Adding the possibility to compute the shortest path length between a starting point and an ending point in a matrix with obstacles. You also have the possibility to compute this path by using diagonals.
    Also adding documentation

---
+++ src/main/java/fr/alexandreladriere/shortestpath/ShortestPath.java
@@ -0,0 +1,13 @@
+package main.java.fr.alexandreladriere.shortestpath;
+
+import main.java.fr.alexandreladriere.shortestpath.models.BreadthFirstSearch;
+
+/**
+ * Main class
+ */
+public class ShortestPath {
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1, 0, 0}, {0, 0, -1}, {0, 0, 2}};
+        System.out.println(BreadthFirstSearch.shortestPathLengthWithDiag(matrix));
+    }
+}
---
+++ src/main/java/fr/alexandreladriere/shortestpath/utils/Constants.java
@@ -0,0 +1,11 @@
+package main.java.fr.alexandreladriere.shortestpath.utils;
+
+/**
+ * Implement constants
+ */
+public final class Constants {
+    public static final int START = 1;
+    public static final int END = 2;
+    public static final int OBSTACLE = -1;
+    public static final int EMPTY = 0;
+}
